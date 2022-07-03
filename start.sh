7c22b78d335892553a31df2554e0d47cbd10e5ec
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-07-03 21:11:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/malik7983/Zee5-Downloader.git /Zee5-Downloader
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Zee5-Downloader
-fi
-cd /Zee5-Downloader
-pip3 install -U -r requirements.txt
-echo "Starting Bot....💥"
 python3 bot.py
